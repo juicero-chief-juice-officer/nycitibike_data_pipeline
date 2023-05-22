d65bd76037279322cf36ad2fab99bb703938de49
Stephen Hazeltine <stephenhazeltine@gmail.com>
2023-05-22 15:13:30 -0400

    Add simple flow for cab data

--- prefect/el_for_hire_vehicle_rides.py
+++ prefect/el_for_hire_vehicle_rides.py
@@ -26,13 +26,12 @@ def el_web_to_gcs_trips(year:int, month:int, color:str) -> None:
     """Main ETL function"""
     dataset_file = f"{color}_tripdata_{year}-{month:02}.parquet"
     dataset_url = f"https://d37ci6vzurychx.cloudfront.net/trip-data/{dataset_file}"
-    
+
     gcs_path = f"aux_data/for_hire_vehicle_trips/{color}/{year}/{month:02}/{dataset_file}"
     
     #download to df
     print(f'read from {dataset_url}')
     df = pd.read_parquet(dataset_url)
-    print(df.columns)
     
     #write df directly to gcs
     write_gcs(df,gcs_path)
@@ -44,17 +43,18 @@ def el_parent_flow_trips(
                     , months: list[int] = [datetime.now().month-1]
                     , colors: list[str] = ['yellow','green']
                     ):
-    try:
-        for mo in months:
-            for yr in years:
-                for clr in colors:
+    print(years)
+    print(months)
+    print(colors)
+    for mo in months:
+        for yr in years:
+            for clr in colors:
+                try:
                     el_web_to_gcs_trips(yr,mo,clr)
-    except Exception as e:
-        print(e)
+                except Exception as e:
+                    print(e)
 if __name__ == '__main__':
-    print('asdljkhfalksdhflaksjd')
     years = list(range(2013,2024))
     months = list(range(1,13))
-    colors = ['yellow','green']
+    colors = ['yellow','green''fhv']
     el_parent_flow_trips(years, months, colors)
-
